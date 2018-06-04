90e0d0a9b5df02f2e7eb39a45788399e556e1a77
Philip Hamner <philiphamner@gmail.com>
2018-06-03 22:16:39 -0700

    added tons of stuff to function buildQuiz()

--- quiz.js
+++ quiz.js
@@ -1,5 +1,38 @@
 (function(){
-	function buildQuiz(){}
+	function buildQuiz(){
+		//we'll need a place to store HTML output
+		const output = [];
+
+		//for each question...
+		myQuestions.forEach(
+			(currentQuestion, questionNumber) => {
+
+				//we'll want to store the list of answer choices
+				const answers = [];
+
+				//and for each available answer
+				for(letter in currentQuestion.answers){
+
+					//...add an HTML radio button
+					answers.push(
+						`<label>
+            				<input type="radio" name="question${questionNumber}" value="${letter}">
+            				${letter} :
+            				${currentQuestion.answers[letter]}
+          				</label>`
+          			);
+				}
+				//add this question and its answers to the output
+				output.push(
+					`<div class="question"> ${currentQuestion.question} </div>
+        			<div class="answers"> ${answers.join('')} </div>`
+        		);
+    		});
+
+			//finally combine our output list into one string of HTML and put it on the page.
+			quizContainer.innerHTML = output.join('');
+		}
+	}
 	
 	function showResults(){}
 
