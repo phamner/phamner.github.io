45363bef6300ea261fd8fda8a7638f3f62193eb1
Philip Hamner <philiphamner@gmail.com>
2018-06-12 22:09:04 -0700

    unit testing "you won"

--- quiz2.js
+++ quiz2.js
@@ -1,3 +1,20 @@
 function check(){
-	document.getElementById("after_submit").style.visibility = "visible";
+	var question1 = document.quiz.question1.value;
+	var question2 = document.quiz.question2.value;
+	var question3 = document.quiz.question3.value;
+	var correct = 0;
+
+	if (question1 == "Providence"){
+		correct++;
+	}
+	if (question2 == "Hartford"){
+		correct++;
+	}
+	if (question3 == "Albany"){
+		correct++;
+	}
+	alert("You won!")
+
+//document.getElementById("after_submit").style.visibility = "visible";
+//document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
 }
