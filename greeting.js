a480501ced51f619cbf68ce428b46981cd5dda22
Philip Hamner <philiphamner@gmail.com>
2018-06-13 20:53:10 -0700

    minor fixes

--- greeting.js
+++ greeting.js
@@ -2,10 +2,6 @@ function helloGuest(){
 	var firstName = document.myform.firstname.value;
 	var lastName = document.myform.lastname.value;
 
-	alert("Hello");
-	//alert("Hello" + firstName + " " + lastName + ", thanks for visiting my website!");
-
-
-
+	alert("Hello " + firstName + " " + lastName + ", thanks for visiting my website!");
 
 }
