66e2df35c471810c620af21be396911f042452e7
Philip Hamner <philiphamner@gmail.com>
2018-06-16 10:37:44 -0700

    added variable to string

--- greeting.js
+++ greeting.js
@@ -3,8 +3,7 @@ function helloGuest(){
 	var lastName = document.myform.lastname.value;
 
 	//alert("Hello " + firstName + " " + lastName + ", thanks for visiting my website!");
-	alert("So long, farewell, au revoir, auf wiedersehen");
-	//alert("h e l l o");
+	alert("So long, farewell, au revoir, auf wiedersehen" + firstName);
 
 //need to get the final greeting message to work in alert() properly.
 
