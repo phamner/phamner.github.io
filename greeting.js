f45ccdd3b2bd5bb86d0a500afaf7be14f0f8a997
Philip Hamner <philiphamner@gmail.com>
2018-06-16 10:39:10 -0700

    replaced var w/ const

--- greeting.js
+++ greeting.js
@@ -1,6 +1,6 @@
 function helloGuest(){
-	var firstName = document.myform.firstname.value;
-	var lastName = document.myform.lastname.value;
+	const firstName = document.myform.firstname.value;
+	const lastName = document.myform.lastname.value;
 
 	//alert("Hello " + firstName + " " + lastName + ", thanks for visiting my website!");
 	alert("So long, farewell, au revoir, auf wiedersehen" + firstName);
