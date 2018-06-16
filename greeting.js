5f1ddd1842af84457060109233ea7c71a94b36f6
Philip Hamner <philiphamner@gmail.com>
2018-06-16 10:40:13 -0700

    eliminated french n german

--- greeting.js
+++ greeting.js
@@ -3,7 +3,7 @@ function helloGuest(){
 	const lastName = document.myform.lastname.value;
 
 	//alert("Hello " + firstName + " " + lastName + ", thanks for visiting my website!");
-	alert("So long, farewell, au revoir, auf wiedersehen" + firstName);
+	alert("So long, farewell" + firstName);
 
 //need to get the final greeting message to work in alert() properly.
 
