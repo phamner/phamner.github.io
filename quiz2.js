2d6f0358d823c4e3953da8c5e6cad31b9564653b
Philip Hamner <philiphamner@gmail.com>
2018-06-17 09:51:05 -0700

    <br>

--- quiz2.js
+++ quiz2.js
@@ -14,6 +14,6 @@ function check(){
 		correct++;
 	}
 
-document.getElementById("after_submit").style.visibility = "visible";
-document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
+	document.getElementById("after_submit").style.visibility = "visible";
+	document.getElementById("number_correct").innerHTML = "You got " + correct + " correct.";
 }
