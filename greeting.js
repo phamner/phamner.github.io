6195f716b5c833196aff107d7611bc7e8987217b
Philip Hamner <philiphamner@gmail.com>
2018-06-17 08:58:11 -0700

    removed extra text

--- greeting.js
+++ greeting.js
@@ -2,10 +2,5 @@ function helloGuest(){
 	const firstName = document.myform.firstname.value;
 	const lastName = document.myform.lastname.value;
 
-	//alert("Hello " + firstName + " " + lastName + ", thanks for visiting my website!");
 	alert("Hi " + firstName + " " + lastName + ", thanks for visiting my website!");
-
-//need to get the final greeting message to work in alert() properly.
-
-
 }
