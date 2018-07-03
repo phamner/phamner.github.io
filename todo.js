94f863fa6de1cc03f3f1d95de8a1c75efb98d31e
Philip Hamner <philiphamner@gmail.com>
2018-07-02 19:25:58 -0700

    copied over

--- todo.js
+++ todo.js
@@ -28,23 +28,15 @@ for (i = 0; i < myNodeList.length; i++){
 	myNodeList[i].appendChild(span);
 }
 
-// const myNodelist = document.getElementsByTagName("LI");
-// var i;
-// for (i = 0; i < myNodelist.length; i++) {
-//   const span = document.createElement("SPAN");
-//   const txt = document.createTextNode("\u00D7");
-//   span.className = "close";
-//   span.appendChild(txt);
-//   myNodelist[i].appendChild(span);
-// }
-
-
-
-
-
-
-
-
+//click on a close button to hide the current list item
+const close = document.getElementByClassName("close");
+const i;
+for (i = 0; i < close.length; i++){
+	close[i].onclick = function(){
+		const div = this.parentElement;
+		div.style.display = "none";
+	}
+}
 
 
 
@@ -54,5 +46,4 @@ list.addEventListener('click', function(ev){
 	if (ev.target.tagName === 'LI'){
 		ev.target.classList.toggle('checked');
 	}
-
 }, false);
